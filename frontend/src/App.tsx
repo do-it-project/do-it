a557394d3416aaa82ded8a91dfd195394bc9443c
Vinicius <vini82001@gmail.com>
2024-04-27 15:16:58 -0300

    feat: add-crud-teste

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div>Olá</div>;
+  return (
+    <div className="h-screen w-full bg-black-1 flex justify-center items-center ">
+      <div className="gap-4 flex flex-wrap">
+        <p className="text-gray-1">Texto primário</p>
+        <p className="text-gray-2">Texto secundário</p>
+        <p className="text-gray-3">Texto alternativo</p>
+        <p className="text-gray-4">Texto alternativo</p>
+        <Button>primary blue</Button>
+        <Button className="bg-blue-2">secondary blue</Button>
+        <Button variant="destructive">error</Button>
+        <div className="bg-black-2 w-44 text-gray-1">bg-2</div>
+        <div className="bg-black-3 w-44 text-gray-1">bg-3</div>
+        <div className="bg-black-4 w-44 text-gray-1">bg-4</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
